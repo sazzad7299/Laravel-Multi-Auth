116c661e8bf87d6bd94348b812a93d3bb6470e9b
Sazzad <sazzadurrahman580@gmail.com>
2022-06-28 21:42:12 +0600

    Complete Dashboard For developer

--- themes/developer/views/layouts/sidebar.blade.php
+++ themes/developer/views/layouts/sidebar.blade.php
@@ -8,27 +8,22 @@
             <i class="fas fa-bars"></i></button>
         <a class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
             href="javascript:void(0)">
-            Tailwind Starter Kit
+            LOGO
         </a>
         <ul class="md:hidden items-center flex flex-wrap list-none">
-            <li class="inline-block relative">
+            {{-- <li class="inline-block relative">
                 <a class="text-blueGray-500 block py-1 px-3" href="#pablo"
                     onclick="openDropdown(event,'notification-dropdown')"><i class="fas fa-bell"></i></a>
                 <div class="hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg mt-1"
                     style="min-width: 12rem;" id="notification-dropdown">
-                    <a href="#pablo"
-                        class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Action</a><a
-                        href="#pablo"
-                        class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Another
-                        action</a><a href="#pablo"
-                        class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Something
-                        else here</a>
-                    <div class="h-0 my-2 border border-solid border-blueGray-100"></div>
-                    <a href="#pablo"
-                        class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Seprated
-                        link</a>
+                    <a href="#pablo" class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Profile</a>
+                    <a href="{{ route('logout') }}" class="py-2 px-4 no-underline hover:underline" onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+
+                <form id="logout-form" action="{{ route('developer.logout') }}" method="POST" class="hidden">
+                    {{ csrf_field() }}
                 </div>
-            </li>
+            </li> --}}
             <li class="inline-block relative">
                 <a class="text-blueGray-500 block" href="#pablo"
                     onclick="openDropdown(event,'user-responsive-dropdown')">
@@ -36,22 +31,19 @@
                         <span
                             class="w-12 h-12 text-sm text-white bg-blueGray-200 inline-flex items-center justify-center rounded-full"><img
                                 alt="..." class="w-full rounded-full align-middle border-none shadow-lg"
-                                src="./assets/img/team-1-800x800.jpg" /></span>
+                                src="{{ asset('themes/developer/img/profile.jpg') }}" /></span>
                     </div>
                 </a>
                 <div class="hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg mt-1"
                     style="min-width: 12rem;" id="user-responsive-dropdown">
                     <a href="#pablo"
-                        class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Action</a><a
-                        href="#pablo"
-                        class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Another
-                        action</a><a href="#pablo"
-                        class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Something
-                        else here</a>
+                        class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Profile</a>
                     <div class="h-0 my-2 border border-solid border-blueGray-100"></div>
-                    <a href="#pablo"
-                        class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Seprated
-                        link</a>
+                    <a href="{{ route('logout') }}" class="py-2 px-4 no-underline hover:underline" onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+                    <form id="logout-form" action="{{ route('developer.logout') }}" method="POST" class="hidden">
+                        {{ csrf_field() }}
+                    </form>
                 </div>
             </li>
         </ul>
@@ -62,7 +54,7 @@
                     <div class="w-6/12">
                         <a class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
                             href="javascript:void(0)">
-                            Tailwind Starter Kit
+                            Logo
                         </a>
                     </div>
                     <div class="w-6/12 flex justify-end">
@@ -74,12 +66,6 @@
                     </div>
                 </div>
             </div>
-            <form class="mt-6 mb-4 md:hidden">
-                <div class="mb-3 pt-0">
-                    <input type="text" placeholder="Search"
-                        class="border-0 px-3 py-2 h-12 border border-solid  border-blueGray-500 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-base leading-snug shadow-none outline-none focus:outline-none w-full font-normal" />
-                </div>
-            </form>
             <ul class="md:flex-col md:min-w-full flex flex-col list-none">
                 <li class="items-center">
                     <a class="text-pink-500 hover:text-pink-600 text-xs uppercase py-3 font-bold block"
@@ -89,67 +75,7 @@
                 <li class="items-center">
                     <a class="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
                         href="#/landing"><i class="fas fa-newspaper text-blueGray-400 mr-2 text-sm"></i>
-                        Landing Page</a>
-                </li>
-                <li class="items-center">
-                    <a class="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
-                        href="#/profile"><i class="fas fa-user-circle text-blueGray-400 mr-2 text-sm"></i>
-                        Profile Page</a>
-                </li>
-                <li class="items-center">
-                    <a class="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
-                        href="#/login"><i class="fas fa-fingerprint text-blueGray-400 mr-2 text-sm"></i>
-                        Login</a>
-                </li>
-                <li class="items-center">
-                    <a class="text-blueGray-300 text-xs uppercase py-3 font-bold block" href="#pablo"><i
-                            class="fas fa-clipboard-list text-blueGray-300 mr-2 text-sm"></i>
-                        Register (soon)</a>
-                </li>
-                <li class="items-center">
-                    <a class="text-blueGray-300 text-xs uppercase py-3 font-bold block" href="#pablo"><i
-                            class="fas fa-tools text-blueGray-300 mr-2 text-sm"></i>
-                        Settings (soon)</a>
-                </li>
-            </ul>
-            <hr class="my-4 md:min-w-full" />
-            <h6 class="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline">
-                Documentation
-            </h6>
-            <ul class="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
-                <li class="inline-flex">
-                    <a class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-                        href="#/documentation/styles"><i
-                            class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"></i>
-                        Styles</a>
-                </li>
-                <li class="inline-flex">
-                    <a class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-                        href="#/documentation/alerts"><i class="fab fa-css3-alt mr-2 text-blueGray-400 text-base"></i>
-                        CSS Components</a>
-                </li>
-                <li class="inline-flex">
-                    <a class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-                        href="#/documentation/vue/alerts"><i class="fab fa-vuejs mr-2 text-blueGray-400 text-base"></i>
-                        VueJS</a>
-                </li>
-                <li class="inline-flex">
-                    <a class="text-blueGray-700 hover:text-blueGray-500  text-sm block mb-4 no-underline font-semibold"
-                        href="#/documentation/react/alerts"><i
-                            class="fab fa-react mr-2 text-blueGray-400 text-base"></i>
-                        React</a>
-                </li>
-                <li class="inline-flex">
-                    <a class="text-blueGray-700 hover:text-blueGray-500  text-sm block mb-4 no-underline font-semibold"
-                        href="#/documentation/angular/alerts"><i
-                            class="fab fa-angular mr-2 text-blueGray-400 text-base"></i>
-                        Angular</a>
-                </li>
-                <li class="inline-flex">
-                    <a class="text-blueGray-700 hover:text-blueGray-500  text-sm block mb-4 no-underline font-semibold"
-                        href="#/documentation/javascript/alerts"><i
-                            class="fab fa-js-square mr-2 text-blueGray-400 text-base"></i>
-                        Javascript</a>
+                        Orders</a>
                 </li>
             </ul>
         </div>
